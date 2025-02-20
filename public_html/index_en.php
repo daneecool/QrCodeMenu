6ee07a69fa475040686ee7f13b49b87b1ed7f2b7
daneecool <daneework93@gmail.com>
2025-02-20 17:25:07 +0900

    slight changes

--- public_html/index_en.php
+++ public_html/index_en.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
   <link rel="stylesheet" href="style.css">
-  <script type="text/javascript" src="app.js" defer></script>
-  <script type="text/javascript" src="encript_passwd.js" defer></script>
+  <script type="text/javascript" src="api/app.js" defer></script>
+  <script type="text/javascript" src="api/encript_passwd.js" defer></script>
 </head>
 
 <body>
